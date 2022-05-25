23f0454d974b9d701005e0c7d5e98d930101e3f7
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-05-25 13:03:36 +0200

    refactoring

--- src/components/KanbanForm.tsx
+++ src/components/KanbanForm.tsx
@@ -4,6 +4,7 @@ export default function KanbanForm(){
     return(
         <div className={'form'}>
             <input type="text" placeholder={'Neue Task'}/>
+            <input type="text" placeholder={'Beschreibung'}/>
             <button>Hinzufügen</button>
         </div>
     )
