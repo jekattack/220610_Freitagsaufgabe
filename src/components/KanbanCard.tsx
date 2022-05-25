97c29a1c95d149b9b32e84a2166402e3659dfc70
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-05-25 16:43:31 +0200

    edit mode

--- src/components/KanbanCard.tsx
+++ src/components/KanbanCard.tsx
@@ -1,7 +1,7 @@
 import './KanbanCard.css'
 import {KanbanItem} from "../service/models";
 import {changeItem, deleteItem, getAllItems} from "../service/apiService";
-import {Dispatch, SetStateAction} from "react";
+import {Dispatch, SetStateAction, useState} from "react";
 
 interface KanbanCardProps{
     infos : KanbanItem
@@ -9,6 +9,10 @@ interface KanbanCardProps{
 }
 
 export default function KanbanCard({infos, onChange} : KanbanCardProps){
+
+    const [edit, setEdit] = useState(false)
+    const [task, setTask] = useState(infos.task)
+    const [description, setDescription] = useState(infos.description)
 
     const removeItem = () =>{
         deleteItem(infos.id)
@@ -37,6 +41,13 @@ export default function KanbanCard({infos, onChange} : KanbanCardProps){
         updateItem()
     }
 
+    const saveItem = () => {
+        infos.task=task
+        infos.description=description
+        updateItem()
+        setEdit(!edit)
+    }
+
     const updateItem = () => {
         changeItem(infos)
             .then(() =>{
@@ -46,22 +57,31 @@ export default function KanbanCard({infos, onChange} : KanbanCardProps){
             .catch(err => console.log(err.message))
     }
 
-    return(
-        <div className={'card'}>
-            <h3>{infos.task}</h3>
-            <p>{infos.description}</p>
-            <div className={'cardBtn'}>
-                {infos.status==="OPEN"?
-                    <button className={'btn'} onClick={()=>removeItem()}>{"<Delete"}</button>
-                    :
-                    <button className={'btn'} onClick={()=>returnItem()}>{"<Back"}</button>
-                }
-                {infos.status==="DONE"?
-                    <button className={'btn'} onClick={()=>removeItem()}>{"Delete>"}</button>
-                    :
-                    <button className={'btn'} onClick={()=>advanceItem()}>{"Next>"}</button>
-                }
+
+        return edit ?
+             <div className={'card_edit'}>
+                 <input type="text" placeholder={'Aufgabe'} value={task} onChange={ev=> setTask(ev.target.value)}/>
+                 <input type="text" placeholder={'Beschreibung'} value={description} onChange={ev=> setDescription(ev.target.value)}/>
+                 <button className={'btn_edit'} onClick={saveItem}>save</button>
             </div>
-        </div>
-    )
+            :
+            <div className={'card'}>
+                <h3>{infos.task}</h3>
+                <p>{infos.description}</p>
+                <div className={'cardBtn'}>
+                    {infos.status==="OPEN"?
+                        <button className={'btn'} onClick={()=>removeItem()}>{"<Delete"}</button>
+                        :
+                        <button className={'btn'} onClick={()=>returnItem()}>{"<Back"}</button>
+                    }
+                    <button className={'btn'} onClick={()=>setEdit(!edit)}>Edit</button>
+                    {infos.status==="DONE"?
+                        <button className={'btn'} onClick={()=>removeItem()}>{"Delete>"}</button>
+                        :
+                        <button className={'btn'} onClick={()=>advanceItem()}>{"Next>"}</button>
+                    }
+                </div>
+            </div>
+
+
 }
